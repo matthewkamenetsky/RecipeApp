49ec6e5a9d478a36ba65838b66ec54e71aeb164d
matthewkamenetsky <kamenetskymatthew@gmail.com>
2025-02-26 10:11:50 -0500

    Adding new files

---
+++ app.js
@@ -0,0 +1,10 @@
+const ingredientInput = document.getElementById('ingredient');
+const searchBtn = document.getElementById('searchBtn');
+const recipeList = document.getElementById('recipeList');
+let url = "https://www.themealdb.com/api/json/v1/1/";
+
+/*potential features: get by name - search.php?s=, random meal - random.php, categories.php, filter.php?i=chicken_breast
+*/
+function GetMeal() {
+
+}
