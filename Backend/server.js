8b93d6b4e5248fc9f0fb8db9682f88425cff853c
matthewkamenetsky <kamenetskymatthew@gmail.com>
2025-09-02 22:34:25 -0400

    more changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const authRoutes = require('./routes/auth');
 const recipeRoutes = require('./routes/recipes');
 require('dotenv').config();
