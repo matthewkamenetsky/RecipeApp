a00922be3c222776b659c8deb216067ad69d4b38
matthewkamenetsky <kamenetskymatthew@gmail.com>
2025-09-02 22:32:31 -0400

    deploying app

--- Backend/server.js
+++ Backend/server.js
@@ -7,9 +7,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(cors());
 app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api', recipeRoutes);
+app.use(express.static(path.join(__dirname, '../Frontend')));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
